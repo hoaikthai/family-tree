4f5dd24c3c0ae638897a548b88e12f5859311db0
Hoai Thai <khanhhoai95@gmail.com>
2026-04-08 18:03:24 +0700

    feat: add useUserPreferences hook

---
+++ src/hooks/useUserPreferences.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import {
+  getPreferences,
+  upsertPreferences,
+  type UserPreferences,
+} from '@/lib/api/userPreferences'
+
+export function useUserPreferences() {
+  return useQuery({
+    queryKey: ['user_preferences'],
+    queryFn: getPreferences,
+  })
+}
+
+export function useUpdateUserPreferences() {
+  const qc = useQueryClient()
+  return useMutation({
+    mutationFn: upsertPreferences,
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['user_preferences'] }),
+  })
+}
